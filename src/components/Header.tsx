ae18137d769e4ff96684a64d9d6df17a3976bcc6
andregustavo1 <andre@andregu.com.br>
2025-04-15 13:51:31 -0300

    up

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -16,7 +16,7 @@ const Header = () => {
   
   return (
     <header 
-      className={`fixed top-0 left-0 w-full z-50 transition-colors duration-400 group 
+      className={`fixed top-0 left-0 w-full z-50 transition-colors duration-300 group 
         ${isHomePage ? 'bg-transparent hover:bg-white' : 'bg-white border-b border-gray-200'}`}
     >
       <div className="container mx-auto px-4 py-5 flex justify-between items-center">
