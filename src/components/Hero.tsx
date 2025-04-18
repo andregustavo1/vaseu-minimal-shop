20cb198f74ac3f03dd548fa8076236f82bab207a
andregustavo1 <andre@andregu.com.br>
2025-04-18 16:41:16 -0300

    up

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -20,12 +20,19 @@ const Hero: FC<HeroProps> = ({ imageUrl }) => {
       <div className="absolute inset-0 bg-black bg-opacity-20 group-hover:bg-opacity-20 transition-all duration-300" />
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="text-center px-4">
-          <h1 className="text-white text-3xl md:text-7xl font-bold tracking-wider mb-4">DROP `1LUMINATE</h1>
+          <h1
+            className="text-white text-3xl md:text-7xl font-bold tracking-wider mb-4"
+            style={{
+              fontFamily: "'Montserrat', sans-serif"
+            }}
+          >
+            DROP `1LUMINATE
+          </h1>
 
           <Link
             to="/shop"
           >
-            <button className="mb-20 px-14 py-4 bg-transparent border-white border-2 rounded-[0.1rem] text-white text-sm uppercase tracking-widest font-medium hover:bg-white hover:text-black duration-300 transition-all">
+            <button className="mb-20 px-14 py-4 bg-transparent border-white border-2 rounded-[0.1rem] text-white text-sm uppercase tracking-widest font-medium hover:bg-white hover:text-black duration-300 transition-all" >
               Shop Now
             </button>
           </Link>
